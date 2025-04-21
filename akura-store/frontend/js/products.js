e99af623ef807e691ad93d42a3fa663c739a5d9f
wajest <112948179+AlexandroTaradin@users.noreply.github.com>
2025-04-21 10:51:18 +0300

    Реализованы карточки товаров *внутри*

---
+++ akura-store/frontend/js/products.js
@@ -0,0 +1,47 @@
+const products = [
+    {
+      id: 1,
+      name: 'AKURA HOODIE BLACK',
+      price: 75,
+      image: 'assets/img/hoodie black.png',
+      description: [
+        'Premium black hoodie with AKURA branding.',
+        'Perfect for urban looks.'
+      ]
+    },
+    {
+      id: 2,
+      name: 'AKURA T-SHIRT BLACK',
+      price: 35,
+      image: 'assets/img/t-shirt-bkack.png',
+      description: [
+        'Oversized fit',
+        '100% cotton 200gsm',
+        'Silkscreen printing',
+        'High quality rhinestones'
+      ]
+    },
+    {
+      id: 3,
+      name: 'AKURA SHORTS BLACK',
+      price: 35,
+      image: 'assets/img/shortsblack.png',
+      description: [
+        'Relaxed black shorts',
+        'Perfect for daily wear',
+        'Soft cotton blend'
+      ]
+    },
+    {
+      id: 4,
+      name: 'AKURA PANTS BLACK',
+      price: 45,
+      image: 'assets/img/pantsblack.png',
+      description: [
+        'Relaxed fit black pants',
+        'Clean silhouette',
+        'Urban-inspired design'
+      ]
+    }
+  ];
+  
