7f7e5ca65bf7d0e8bd6ce9cb055a3ee596e68947
fuunzap <113061535+fuunzap@users.noreply.github.com>
2025-05-19 00:32:29 +0300

    Обновлена база данных
    Добавлены новые таблицы и связки между ними

--- akura_store.sql
+++ akura_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 07 2025 г., 21:55
+-- Время создания: Май 18 2025 г., 23:31
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- База данных: `akura_store`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(2, 'Футболки'),
+(1, 'Худи'),
+(4, 'Шорты'),
+(3, 'Штаны');
 
 -- --------------------------------------------------------
 
@@ -47,11 +68,7 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `full_name`, `email`, `address`, `city`, `zip`, `delivery_method`, `items`, `created_at`, `user_id`, `status`, `archived`) VALUES
-(5, 'Test', 'ifsshfhsh@gmail.com', 'fsjksjll', 'fsfa', '35453', 'Courier', '[{\"id\":\"3\",\"name\":\"AKURA SHORTS BLACK\",\"price\":35,\"quantity\":1,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/shortsblack.png\",\"size\":\"XL\"}]', '2025-05-04 18:47:13', 5, 'Подтверждён', 1),
-(6, 'Oleksandr Taradin', 'taradin@gmail.com', 'Narva,Tempo 19/2', 'Narva', '5333', 'Courier', '[{\"id\":\"4\",\"name\":\"AKURA T-SHIRT BLACK\",\"price\":35,\"quantity\":4,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/t-shirt-black.png\",\"size\":\"XL\"},{\"id\":\"4\",\"name\":\"AKURA T-SHIRT BLACK\",\"price\":35,\"quantity\":2,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/t-shirt-black.png\",\"size\":\"L\"}]', '2025-05-04 19:15:34', 5, 'Оформлен возврат', 1),
-(7, 'Tdaada', 'dad@gmail.com', 'kjfshfjksha', 'Narva', '425525', 'Pickup', '[{\"id\":\"4\",\"name\":\"AKURA T-SHIRT BLACK\",\"price\":35,\"quantity\":3,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/t-shirt-black.png\",\"size\":\"M\"}]', '2025-05-04 19:24:55', NULL, 'Отменён', 1),
-(8, 'Jegor Jakovlev', 'tetst@gmail.com', 'Narva Astri', 'Narca', '2442242', 'Pickup', '[{\"id\":\"3\",\"name\":\"AKURA SHORTS BLACK\",\"price\":35,\"quantity\":1,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/shortsblack.png\",\"size\":\"L\"}]', '2025-05-04 19:37:09', 5, 'Готовится к отправке', 1),
-(0, 'Artem', 'admin@it.com', 'krenholmi 34', 'narva', '40122', 'Courier', '[{\"id\":\"1\",\"name\":\"AKURA HOODIE BLACK\",\"price\":75,\"quantity\":1,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/hoodie%20black.png\",\"size\":\"L\"}]', '2025-05-06 19:00:23', 6, 'Новый', 1);
+(10, 'Artem', 'slappy@it.com', 'krenholmi 34', 'narva', '40122', 'Courier', '[{\"id\":\"2\",\"name\":\"AKURA PANTS BLACK\",\"price\":45,\"quantity\":1,\"image_url\":\"http://127.0.0.1:5500/frontend/assets/img/pantsblack.png\",\"size\":\"XL\"}]', '2025-05-18 20:06:49', 7, 'Новый', 0);
 
 -- --------------------------------------------------------
 
@@ -63,8 +80,8 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `category` varchar(100) DEFAULT NULL,
-  `size` varchar(10) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `size_id` int(11) DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL,
   `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -73,10 +90,10 @@ CREATE TABLE `products` (
 -- Дамп данных таблицы `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`, `category`, `size`, `image_url`, `description`) VALUES
-(1, 'AKURA HOODIE BLACK', 75.00, 'Худи', 'XX', 'assets/img/hoodie black.png', 'Premium black hoodie with AKURA branding. Perfect for urban looks.'),
-(2, 'AKURA PANTS BLACK', 45.00, 'Штаны', 'L', 'assets/img/pantsblack.png', 'Relaxed fit black pants. Clean silhouette. Urban-inspired design.'),
-(3, 'AKURA SHORTS BLACK', 35.00, 'Шорты', 'M', 'assets/img/shortsblack.png', 'Relaxed black shorts. Perfect for daily wear. Soft cotton blend.');
+INSERT INTO `products` (`id`, `name`, `price`, `category_id`, `size_id`, `image_url`, `description`) VALUES
+(1, 'AKURA HOODIE BLACK', 75.00, 1, 4, 'assets/img/shortsblack.png', 'Premium black hoodie with AKURA branding. Perfect for urban looks.'),
+(2, 'AKURA PANTS BLACK', 45.00, 3, 2, 'assets/img/pantsblack.png', 'Relaxed fit black pants. Clean silhouette. Urban-inspired design.'),
+(3, 'AKURA SHORTS BLACK', 35.00, 4, 2, 'assets/img/shortsblack.png', 'Relaxed black shorts. Perfect for daily wear. Soft cotton blend.');
 
 -- --------------------------------------------------------
 
@@ -96,8 +113,28 @@ CREATE TABLE `promo_codes` (
 --
 
 INSERT INTO `promo_codes` (`id`, `code`, `discount`, `created_at`) VALUES
-(4, 'SECRET', 25.00, '2025-05-07 19:13:35'),
-(5, 'KSYSHA', 55.00, '2025-05-07 19:20:44');
+(4, 'SECRET', 25.00, '2025-05-07 19:13:35');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sizes`
+--
+
+CREATE TABLE `sizes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `sizes`
+--
+
+INSERT INTO `sizes` (`id`, `name`) VALUES
+(3, 'L'),
+(2, 'M'),
+(1, 'S'),
+(4, 'XL');
 
 -- --------------------------------------------------------
 
@@ -111,7 +148,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `role` enum('User','Admin','Manager') NOT NULL DEFAULT 'User'
+  `role` enum('User','Manager','Admin') NOT NULL DEFAULT 'User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -119,17 +156,33 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `role`) VALUES
-(6, 'Artem', 'artem@it.com', '$2b$10$S/T/bHvZWfxZPXuMl9WU/Oq8cswmHWkwTlchSmluwZVH1oYJProwG', '2025-05-06 08:37:13', 'Admin');
+(6, 'Artem', 'artem@it.com', '$2b$10$S/T/bHvZWfxZPXuMl9WU/Oq8cswmHWkwTlchSmluwZVH1oYJProwG', '2025-05-06 08:37:13', 'Manager'),
+(7, 'sasko', 'sasko@www.team', '$2b$10$oBLtDRawHz6mUmm44hS8Z.RZM/g9KiN0wP.gElqrSDcJhbXBu0xqy', '2025-05-18 16:15:25', 'User');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_category` (`category_id`),
+  ADD KEY `fk_size` (`size_id`);
 
 --
 -- Индексы таблицы `promo_codes`
@@ -137,6 +190,13 @@ ALTER TABLE `products`
 ALTER TABLE `promo_codes`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `code` (`code`);
+
+--
+-- Индексы таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Индексы таблицы `users`
@@ -150,22 +210,53 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `promo_codes`
 --
 ALTER TABLE `promo_codes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `sizes`
+--
+ALTER TABLE `sizes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `fk_products_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_products_size` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_size` FOREIGN KEY (`size_id`) REFERENCES `sizes` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
